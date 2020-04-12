a8be39201a7b007cf5fd4472fcae6a978a0122f6
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-04-12 21:45:47 +0300

    Release version

---
+++ index.js
@@ -0,0 +1,13 @@
+x = 1;
+y = 2;
+console.log(x);
+
+x = y;
+console.log(x);
+
+some_list = [1, 2, 3, 4]; //[1, 2, 3, 4]
+some_list_2 = some_list; //[1, 2, 3, 4]
+
+some_list.push(11);
+console.log(some_list);
+console.log(some_list_2);
