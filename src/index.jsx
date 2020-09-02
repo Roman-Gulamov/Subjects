e164ec4833fcfcb955a0fdd403dd00468e9b97b8
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-02 03:36:13 +0300

    Imported fonts and styles file

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+
 import { App } from './Components/App';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/fonts/stylesheet.css';
+import './index.scss';
+
 
 ReactDOM.render(<App />,document.getElementById('root'));
