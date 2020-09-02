c203b05b08e648b13c7f5c9f0b778205e98ee0dd
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-02 03:38:14 +0300

    Site basis

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { RouteDistrict } from './Router/RouteDistrict';
+import './app.scss';
+
 
 export const App = () => {
     return (
-        <>
-        <h1>Hi</h1>
-        </>
+        <div className="list">
+            <div className="container">
+                    <h1 className="list-title title-center">Федеральные округа Российской Федерации</h1>
+                <div className="list-districts">
+                    <RouteDistrict />
+                </div>
+            </div>
+        </div>
     );
 }
