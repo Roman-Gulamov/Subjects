8c168df61e52bd94fb2e3f70457e214d9d18ba05
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-03 03:53:02 +0300

    Changed page routing

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { HashRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+
+import { RouteBranches } from './Router/RouteBranches';
 import { RouteDistrict } from './Router/RouteDistrict';
 import './app.scss';
 
 
 export const App = () => {
     return (
-        <div className="list">
-            <div className="container">
-                    <h1 className="list-title title-center">Федеральные округа Российской Федерации</h1>
-                <div className="list-districts">
-                    <RouteDistrict />
+        <div className="choise container list-links">
+            <Router>
+                <div className="choise-text d-flex justify-content-around flex-wrap pt-4 text-center">
+                    <Link to="/districts" className="text-danger">Округа Российской Федерации</Link>
+                    <Link to="/branches" className="text-danger">Филиалы</Link>
                 </div>
-            </div>
+
+                <Switch>
+                    <Route path='/districts' component={RouteDistrict}/>
+                    <Route path='/branches' component={RouteBranches}/>
+                    <Redirect to="/" />
+                </Switch>
+            </Router>
         </div>
     );
 }
