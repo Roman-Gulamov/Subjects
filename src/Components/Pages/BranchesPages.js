3cdce6ed83395af209961bc736b0b0c13dd7352b
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-03 03:55:57 +0300

    All pages are merged

---
+++ src/Components/Pages/BranchesPages.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+import { DataMap } from '../Map/DataMap';
+
+
+export const FirstBranch = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Первый филиал</title>
+            </Helmet>
+            <DataMap pageTitle="Филиал №1"/>
+        </>
+    )
+}
+
+export const SecondBranch = () => {
+        return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Второй филиал</title>
+            </Helmet>
+            <DataMap pageTitle="Филиал №2"/>
+        </>
+    )
+}
+
+export const ThirdBranch = () => {
+        return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Третий филиал</title>
+            </Helmet>
+            <DataMap pageTitle="Филиал №3"/>
+        </>
+    )
+}
+
+export const FourthBranch = () => {
+        return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Четвёртый филиал</title>
+            </Helmet>
+            <DataMap pageTitle="Филиал №4"/>
+        </>
+    )
+}
+
+export const FifthBranch = () => {
+        return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Пятый филиал</title>
+            </Helmet>
+            <DataMap pageTitle="Филиал №5"/>
+        </>
+    )
+}
+
+export const SixthBranch = () => {
+        return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Шестой филиал</title>
+            </Helmet>
+            <DataMap pageTitle="Филиал №6"/>
+        </>
+    )
+}
+
+export const SeventhBranch = () => {
+        return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Сеьмой филиал</title>
+            </Helmet>
+            <DataMap pageTitle="Филиал №7"/>
+        </>
+    )
+}
+
+export const EighthBranch = () => {
+        return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Восьмой филиал</title>
+            </Helmet>
+            <DataMap pageTitle="Филиал №8"/>
+        </>
+    )
+}
---
+++ src/Components/Pages/DistrictsPages.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { DataMap } from '../Map/DataMap';
+
+
+export const Central = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Центральный федеральный округ</title>
+            </Helmet>
+            <DataMap pageTitle="Центральный федеральный округ"/>
+        </>
+    )
+}
+
+export const FarEast = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Дальневосточный федеральный округ</title>
+            </Helmet>
+            <DataMap pageTitle="Дальневосточный федеральный округ"/>
+        </>
+    )
+}
+
+export const NorthCaucasian = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Северо-Кавказский федеральный округ</title>
+            </Helmet>
+            <DataMap pageTitle="Северо-Кавказский федеральный округ"/>
+        </>
+    )
+}
+
+export const NorthWestern = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Северо-западный федеральный округ</title>
+            </Helmet>
+            <DataMap pageTitle="Северо-западный федеральный округ"/>
+        </>
+    )
+}
+
+export const Siberia = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Сибирский федеральный округ</title>
+            </Helmet>
+            <DataMap pageTitle="Сибирский федеральный округ"/>
+        </>
+    )
+}
+
+export const Southern = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Южный федеральный округ</title>
+            </Helmet>
+            <DataMap pageTitle="Южный федеральный округ"/>
+        </>
+    )
+}
+
+export const Ural = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Уральский федеральный округ</title>
+            </Helmet>
+            <DataMap pageTitle="Уральский федеральный округ"/>
+        </>
+    )
+}
+
+export const Volga = () => {
+    return (
+        <>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Приволжский федеральный округ</title>
+            </Helmet>
+            <DataMap pageTitle="Приволжский федеральный округ"/>
+        </>
+    )
+}
