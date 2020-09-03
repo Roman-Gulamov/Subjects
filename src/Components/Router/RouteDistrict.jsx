4f527cf8e6aa7a8dca741c388e6f8b2136e825af
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-03 03:54:05 +0300

    Changing paths

--- src/Components/Router/RouteDistrict.jsx
+++ src/Components/Router/RouteDistrict.jsx
@@ -1,41 +1,57 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { HashRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
 
-import { Central } from '../Pages/Central';
-import { NorthWestern } from '../Pages/NorthWestern';
-import { Southern } from '../Pages/Southern';
-import { NorthCaucasian } from '../Pages/NorthCaucasian';
-import { Volga } from '../Pages/Volga';
-import { Siberia } from '../Pages/Siberia';
-import { FarEast } from '../Pages/FarEast';
-import { Ural } from '../Pages/Ural';
+import { 
+    Central,
+    NorthWestern,
+    Southern,
+    NorthCaucasian,
+    Volga,
+    Siberia,
+    FarEast,
+    Ural
+} from '../Pages/DistrictsPages';
 
 
 export const RouteDistrict = () => {
     return (
-        <Router>
-            <div className="d-flex justify-content-around flex-wrap">
-                <Link to="/central">Центральный</Link>
-                <Link to="/north-western">Северо-Западный</Link>
-                <Link to="/southern">Южный</Link>
-                <Link to="/north-caucasian">Северо-Кавказский</Link>
-                <Link to="/volga">Приволжский</Link>
-                <Link to="/ural">Уральский</Link>
-                <Link to="/siberia">Сибирский</Link>
-                <Link to="/far-east">Дальневосточный</Link>
-            </div>
+        <>
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Список субъектов</title>
+        </Helmet>
+        <div className="list-districts">
+            <div className="container">
+                    <h1 className="list-title title-center">Федеральные округа Российской Федерации</h1>
+                <div className="list-links">
+                    <Router>
+                        <div className="d-flex justify-content-around flex-wrap">
+                            <Link to="/districts/central">Центральный</Link>
+                            <Link to="/districts/north-western">Северо-Западный</Link>
+                            <Link to="/districts/southern">Южный</Link>
+                            <Link to="/districts/north-caucasian">Северо-Кавказский</Link>
+                            <Link to="/districts/volga">Приволжский</Link>
+                            <Link to="/districts/ural">Уральский</Link>
+                            <Link to="/districts/siberia">Сибирский</Link>
+                            <Link to="/districts/far-east">Дальневосточный</Link>
+                        </div>
 
-            <Switch>
-                <Route path='/central' component={Central}/>
-                <Route path='/north-western' component={NorthWestern}/>
-                <Route path='/southern' component={Southern}/>
-                <Route path='/north-caucasian' component={NorthCaucasian}/>
-                <Route path='/volga' component={Volga}/>
-                <Route path='/ural' component={Ural}/>
-                <Route path='/siberia' component={Siberia}/>
-                <Route path='/far-east' component={FarEast}/>
-                <Redirect to="/" />
-            </Switch>
-        </Router>
+                        <Switch>
+                            <Route path='/districts/central' component={Central}/>
+                            <Route path='/districts/north-western' component={NorthWestern}/>
+                            <Route path='/districts/southern' component={Southern}/>
+                            <Route path='/districts/north-caucasian' component={NorthCaucasian}/>
+                            <Route path='/districts/volga' component={Volga}/>
+                            <Route path='/districts/ural' component={Ural}/>
+                            <Route path='/districts/siberia' component={Siberia}/>
+                            <Route path='/districts/far-east' component={FarEast}/>
+                            <Redirect to="/districts" />
+                        </Switch>
+                    </Router>
+                </div>
+            </div>
+        </div>
+        </>
     )
 }
