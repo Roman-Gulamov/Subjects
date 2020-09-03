eea0bb7b780971a559610b0692ebaa30ef9ba1de
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-09-03 03:54:45 +0300

    Changing paths

---
+++ src/Components/Map/sortData.js
@@ -0,0 +1,17 @@
+import { sortRef } from './ref';
+
+export const sortData = () => {
+    let sort = sortRef.current;
+    for (let i = 0; i < sort.children.length; i++) {
+        for (let j = i; j < sort.children.length; j++) {
+            if (+sort.children[i].getAttribute('data-srt') > +sort.children[j].getAttribute('data-srt')) {
+                let replacedNode = sort.replaceChild(sort.children[j], sort.children[i]);
+                insertBefore(replacedNode, sort.children[i]);
+            }
+        }
+    }
+}
+
+const insertBefore = (elem, refElem) => {
+    return refElem.parentNode.insertBefore(elem, refElem.nextSibling);
+}
